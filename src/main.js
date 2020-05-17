ec2d0cc1cec7269d07b46e547f1a6abd7c54f870
0xMilk <1144502582@qq.com>
2020-05-17 17:49:19 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router'
+import store from './store'
+//导入全局样式表
+
+import './assets/css/global.css'
+// highlight.js代码高亮指令
+import Highlight from './assets/js/highlight';
+
+Vue.use(Highlight);
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
