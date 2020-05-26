24c5d33d14d25329f251a4f7b3d6a4754203bd01
0xMilk <1144502582@qq.com>
2020-05-26 10:16:00 +0800

    修改文件

--- src/network/request.js
+++ src/network/request.js
@@ -2,19 +2,12 @@ import axios from 'axios'
 
 //网络请求
 
-const domain = 'http://localhost:8080';
-
-export function domian() {
-  return domain;
-}
-
 export function request(config) {
   //1. 创建axios实例
   const instance = axios.create({
     //写你的基本地址: 服务器ip和端口
-    baseURL: 'http://localhost:8080',
-    // timeout: 5000,
-
+    baseURL: '/api',
+    timeout: 5000,
     //跨域
     crossDomain: true
 
@@ -23,4 +16,3 @@ export function request(config) {
   //2. 发送网络请求,instance本身就是一个Promise类型可以直接返回
   return instance(config)
 }
-
