c78c77190112fe7f31c17507d7f2b80b130e690c
0xMilk <1144502582@qq.com>
2020-05-26 16:23:11 +0800

    修改路径

--- src/network/request.js
+++ src/network/request.js
@@ -6,7 +6,7 @@ export function request(config) {
   //1. 创建axios实例
   const instance = axios.create({
     //写你的基本地址: 服务器ip和端口
-    baseURL: '/api',
+    baseURL: 'http://localhost:9192/api',
     timeout: 5000,
     //跨域
     crossDomain: true
