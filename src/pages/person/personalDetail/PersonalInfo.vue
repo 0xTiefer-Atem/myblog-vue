98e7c8f81b207bfa606200a00580e6457e15e77e
0xMilk <1144502582@qq.com>
2020-05-28 11:15:36 +0800

    重新设计的页面

---
+++ src/pages/person/personalDetail/PersonalInfo.vue
@@ -0,0 +1,64 @@
+<template>
+    <el-card class="work-card" shadow="never">
+        <el-page-header @back="goBack" :content="pageHeader">
+        </el-page-header>
+        <div class="ql-snow">
+            <div class="ql-editor" v-html="workDetail.blogContent" v-hljs></div>
+        </div>
+    </el-card>
+</template>
+
+<script>
+    import {request} from "../../../network/request";
+
+    export default {
+        name: "WorkInfo",
+        data() {
+            return {
+                workDetail: {},
+                pageHeader: '',
+            }
+        },
+        activated() {
+            this.getBlogDetail()
+        },
+        methods: {
+
+            //获取具体博客内容
+
+            getBlogDetail() {
+                let id = this.$route.query.id;
+                if(id === '002') {
+                    this.pageHeader = '项目经历'
+                }else  {
+                    this.pageHeader = '教育经历'
+                }
+                console.log(id);
+                request({
+                    url: '/blog/selectOne?id=' + id + '&status=2'
+                }).then(res => {
+                    let resData = res.data;
+                    console.log(resData);
+                    if(resData.status === 2000) {
+                        this.workDetail = resData.result.data;
+                    }
+                })
+            },
+            //返回上一级页面
+            goBack() {
+                this.$router.back();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .work-card {
+        border-radius: 30px;
+    }
+
+    .work-card-header {
+        font-weight: bold;
+        text-align: left;
+    }
+</style>
