a7195e23565cf79632840939389b91e37f5acbdb
0xMilk <1144502582@qq.com>
2020-07-01 19:04:46 +0800

    修改样式

--- src/pages/person/personalDetail/PersonalInfo.vue
+++ src/pages/person/personalDetail/PersonalInfo.vue
@@ -1,11 +1,11 @@
 <template>
-    <el-card class="work-card" shadow="never">
-        <el-page-header @back="goBack" :content="pageHeader">
-        </el-page-header>
-        <div class="ql-snow">
-            <div class="ql-editor" v-html="workDetail.blogContent" v-hljs></div>
-        </div>
-    </el-card>
+  <el-card class="work-card" shadow="never">
+    <el-page-header @back="goBack" :content="pageHeader">
+    </el-page-header>
+    <div class="ql-snow">
+      <div class="ql-editor" v-html="workDetail.blogContent" v-hljs></div>
+    </div>
+  </el-card>
 </template>
 
 <script>
@@ -28,9 +28,9 @@
 
             getBlogDetail() {
                 let id = this.$route.query.id;
-                if(id === '002') {
+                if (id === '002') {
                     this.pageHeader = '项目经历'
-                }else  {
+                } else {
                     this.pageHeader = '教育经历'
                 }
                 console.log(id);
@@ -39,7 +39,7 @@
                 }).then(res => {
                     let resData = res.data;
                     console.log(resData);
-                    if(resData.status === 2000) {
+                    if (resData.status === 2000) {
                         this.workDetail = resData.result.data;
                     }
                 })
@@ -53,12 +53,12 @@
 </script>
 
 <style scoped>
-    .work-card {
-        border-radius: 30px;
-    }
+  .work-card {
+    border-radius: 30px;
+  }
 
-    .work-card-header {
-        font-weight: bold;
-        text-align: left;
-    }
+  .work-card-header {
+    font-weight: bold;
+    text-align: left;
+  }
 </style>
