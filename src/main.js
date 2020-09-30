93bbf5696b138e98000ed0c5f08efa949bc0ab93
Wq <1144502582@qq.com>
2020-09-30 19:29:47 +0800

    引入 mavonEditor 组件

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import store from './store'
-//导入全局样式表
 
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+// use
+Vue.use(mavonEditor)
+
+//导入全局样式表
 import './assets/css/global.css'
+
 // highlight.js代码高亮指令
 import Highlight from './assets/js/highlight';
 
