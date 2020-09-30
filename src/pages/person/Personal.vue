b935ad54a3e351b14ba38edd2081bd815ec4f998
Wq <1144502582@qq.com>
2020-09-30 19:29:04 +0800

    修改个人简介页面布局

--- src/pages/person/Personal.vue
+++ src/pages/person/Personal.vue
@@ -1,121 +1,128 @@
 <template>
-    <el-card class="personal-card" shadow="never">
-        <el-row :gutter="24">
-            <el-col :span="8" :offset="2" v-for="(info, index) in personalInfoList" :key="index">
-                <el-card  :body-style="{ padding: '0px' }"
-                          class="article-card"
-                          shadow="hover">
-                    <img src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" class="image">
-                    <el-row>
-                        <el-col>
-                            <div>
-                                <el-card class="article-info-card" @click.native="personalDetailClick(index)" shadow="never">
-                                    <div slot="header" class="article-title">
-                                        {{info.blogTitle}}
-                                    </div>
-                                    <div class="article-cover-text">
-                                        {{info.blogType}}
-                                    </div>
-                                    <el-divider></el-divider>
-                                    <el-row :grunt="24">
-                                        <el-col :span="12" :offset="12">
-                                            <div>
-                                                <el-tag>{{info.createTime}}</el-tag>
-                                            </div>
-                                        </el-col>
-                                    </el-row>
-                                </el-card>
-                            </div>
-                        </el-col>
-                    </el-row>
+  <el-card class="personal-card" shadow="never">
+    <el-row :gutter="24">
+      <el-col :span="8" :offset="2" v-for="(info, index) in personalInfoList" :key="index">
+        <el-card :body-style="{ padding: '0px' }"
+                 class="article-card"
+                 shadow="hover">
+          <img src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" class="image">
+          <el-row>
+            <el-col>
+              <div>
+                <el-card class="article-info-card" @click.native="personalDetailClick(index)" shadow="never">
+                  <div slot="header" class="article-title">
+                    {{ info.blogTitle }}
+                  </div>
+                  <div class="article-cover-text">
+                    {{ info.blogType }}
+                  </div>
+                  <el-divider></el-divider>
+                  <el-row :grunt="24">
+                    <el-col :span="12" :offset="12">
+                      <div>
+                        <el-tag>{{ dateFormat(info.createTime) }}</el-tag>
+                      </div>
+                    </el-col>
+                  </el-row>
                 </el-card>
+              </div>
             </el-col>
-        </el-row>
-    </el-card>
+          </el-row>
+        </el-card>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
-    import {request} from "../../network/request";
+import {request} from "@/network/request";
+import moment from 'moment'
 
-    export default {
-        name: "Personal",
-        data() {
-            return {
-                personalInfoList: []
-            }
-        },
-        activated() {
-            request({
-                url: '/blog/selectSpecialBlog',
-                method: 'get'
-            }).then( res => {
-                let resData = res.data;
-                console.log(resData);
-                if(resData.status === 2000) {
-                    this.personalInfoList = resData.result.data;
-                }else {
-                    this.$message.error("文章查询失败")
-                }
-            })
-        },
-        methods: {
-            personalDetailClick(index) {
-                let query = {}
-                if(index%2 === 0) {
-                    query.id = '002'
-                }else {
-                    query.id = '003'
-                }
-
-                this.$router.push({
-                    path: "/my-blog/personal-info",
-                    query
-                });
-            }
-        }
+export default {
+  name: "Personal",
+  data() {
+    return {
+      personalInfoList: []
     }
+  },
+  activated() {
+    request({
+      url: '/blog/query/special',
+      method: 'get'
+    }).then(res => {
+      let resData = res.data;
+      console.log(resData);
+      if (resData.status === 200) {
+        this.personalInfoList = resData.result.data;
+      } else {
+        this.$message.error("文章查询失败")
+      }
+    })
+  },
+  methods: {
+    /*日期处理*/
+    dateFormat(date) {
+      return moment(date).format("YYYY-MM-DD HH:mm")
+    },
+
+    personalDetailClick(index) {
+      let query = {}
+      if (index % 2 === 0) {
+        query.id = '002'
+      } else {
+        query.id = '003'
+      }
+
+      this.$router.push({
+        path: "/my-blog/personal-info",
+        query
+      });
+    }
+  }
+}
 </script>
 
 <style scoped>
-    .personal-card {
-        border-radius: 30px;
-    }
-    .image {
-        width: 100%;
-        display: block;
-    }
+.personal-card {
+  border-radius: 30px;
+}
 
-    .article-card {
-        border-radius: 30px;
-        margin: 10px;
-    }
+.image {
+  width: 100%;
+  display: block;
+}
 
-    .article-list-card{
-        border-radius: 30px;
-    }
+.article-card {
+  border-radius: 30px;
+  margin: 10px;
+}
 
-    .article-cover-text {
-        margin: 10px;
-        text-indent:34px;
-        text-align:left;
-        word-break:break-all;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
+.article-list-card {
+  border-radius: 30px;
+}
 
-    }
+.article-cover-text {
+  margin: 10px;
+  text-indent: 34px;
+  text-align: left;
+  word-break: break-all;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 
-    .article-title {
-        font-weight: bold;
-        text-align:left;
-        word-break:break-all;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-    }
+}
+
+.article-title {
+  font-weight: bold;
+  text-align: left;
+  word-break: break-all;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
 
 </style>
